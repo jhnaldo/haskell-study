d32d9ff7f033bf0086d14b62798edcb153a40bf4
Jihyeok Park <jhpjhp0223@gmail.com>
2016-10-01 23:46:01 +0900

    solve hw2

--- hw2/HW02.hs
+++ hw2/HW02.hs
@@ -23,43 +23,62 @@ colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
 -- Get the number of exact matches between the actual code and the guess
 exactMatches :: Code -> Code -> Int
-exactMatches = undefined
+exactMatches left right = length $ filter (uncurry (==)) $ zip left right
 
 -- Exercise 2 -----------------------------------------
 
 -- For each peg in xs, count how many times is occurs in ys
 countColors :: Code -> [Int]
-countColors = undefined
+countColors code = map (\peg -> length $ filter (== peg) code) colors
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches left right = foldl minSum 0 pairs
+  where
+    pairs = zip (countColors left) (countColors right)
+    minSum n (l, r) = n + min l r
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove left right = Move right exact nonExact
+  where
+    nonExact = matches left right - exact
+    exact = exactMatches left right
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent move@(Move ans _ _) code = getMove code ans == move
 
 -- Exercise 5 -----------------------------------------
 
 filterCodes :: Move -> [Code] -> [Code]
-filterCodes = undefined
+filterCodes move codes = filter (isConsistent move) codes
 
 -- Exercise 6 -----------------------------------------
 
 allCodes :: Int -> [Code]
-allCodes = undefined
+allCodes n
+  | n <= 0    = [[]]
+  | otherwise = foldr merge [] colors
+  where
+    merge color lst = foldr (colorAppend color) lst $ allCodes $ n - 1
+    colorAppend color code l = (color : code) : l
 
 -- Exercise 7 -----------------------------------------
 
 solve :: Code -> [Move]
-solve = undefined
+solve code = reverse $ subSolve [] $ allCodes $ len
+  where
+    subSolve _ [] = undefined
+    subSolve lst (first : remain) = next lst (getMove code first) remain
+    next lst move @ (Move _ l _) remain = case () of
+      () | l == len -> move : lst
+         | otherwise -> subSolve (move : lst) (filterCodes move remain)
+    len = length code
+
 
 -- Bonus ----------------------------------------------
 
